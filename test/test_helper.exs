bf538f7f1d0d2d2f986b07da1cf7ee4a2e2b2139
Nícolas Silveira <nicolas.knoxx@gmail.com>
2025-08-22 01:37:02 -0300

    Run mix format

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,6 +3,4 @@ ExUnit.start()
 # Exclude performance, stress, and benchmark tests by default
 # Run them explicitly with: mix test --include performance
 # or: mix test --include stress --include recovery --include benchmark
-ExUnit.configure(
-  exclude: [:performance, :stress, :recovery, :benchmark, :grpc_benchmark]
-)
+ExUnit.configure(exclude: [:performance, :stress, :recovery, :benchmark, :grpc_benchmark])
